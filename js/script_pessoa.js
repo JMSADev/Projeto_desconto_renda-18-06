bef87b90366dee2dbd17b42985b3c0ee281b3df5
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-19 19:51:46 -0300

    atualizando

--- js/script_pessoa.js
+++ js/script_pessoa.js
@@ -1,32 +1,3 @@
-/* módulo do array que vai conter os dado inseridos no form
-const newForm = document.querySelector('#form')
-const nome = document.querySelector('#nome')
-const idade = document.querySelector('#idade')
-const renda = document.querySelector('#renda')
-
-const pessoas = []
-
-newForm.addEventListener('submit', (evt) =>{
-evt.preventDefault()
-
-const nomePessoa = nome.value
-const idadePessoa = Number(idade.value)
-const rendaPessoa = Number(renda.value)
-
-pessoas.push({
-    nome: nomePessoa,
-    idade: idadePessoa,
-    renda: rendaPessoa,
-})
-
-
-})
- 
-export {pessoas}
-
-TODA ESSA LINHA ACIMA FOI EU TENTANDO FAZER DO JEITO QUE APRENDI NA QUESTÃO 4, MAS PAREI AO PERCEBER QUE SERIA DIFERENTE AO DECORRER DA AULA.
-*/
-
 const pessoas = []
 
 const formPessoa = document.querySelector('#form-pessoa')
